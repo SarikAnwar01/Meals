795037c87c5cc8b4eb5bbcbfb7abef2a96ea6631
SarikAnwar01 <sarikanwar007@gmail.com>
2022-04-07 18:57:51 +0545

    useeffect for the cart button animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,35 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
 const HeaderCartButton = props => {
+    const [btnIsHighLighted, setBtnIsHighLighted] = useState(false);
     const cartCtx = useContext(CartContext);
+    const { items } = cartCtx;
 
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
+
+
+    const btnClasses = `${classes.button} ${btnIsHighLighted ? classes.bump : ""}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnIsHighLighted(true);
+        const timer = setTimeout(() => {
+            setBtnIsHighLighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
     return (
         <Fragment>
-            <button className={classes.button} onClick={props.onClick}>
+            <button className={btnClasses} onClick={props.onClick}>
                 <span className={classes.icon}>
                     <CartIcon />
                 </span>
