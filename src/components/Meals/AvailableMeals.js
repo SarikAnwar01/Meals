48eaaee58956cfa3612943148a55da1ff1266dcc
Sarik Anwar <sarikanwar007@gmail.com>
2022-04-16 18:24:15 +0545

    custom Loader

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -2,15 +2,19 @@ import { useEffect, useState } from 'react';
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
-
+import { BarWave } from 'react-cssfx-loading/lib';
 
 const AvailableMeals = () => {
     const [meals, setMeals] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
     useEffect(() => {
         const fetchMeals = async () => {
 
             const response = await fetch("https://react-http-d404d-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json");
+            // console.log(response);{many data in this object}
             const responseData = await response.json();
+            // console.log(responseData);{convert to json}
             const loadedMeals = [];
             for (const key in responseData) {
                 loadedMeals.push({
@@ -21,9 +25,17 @@ const AvailableMeals = () => {
                 });
             }
             setMeals(loadedMeals);
+            setIsLoading(false);
         };
         fetchMeals();
     }, []);
+    if (isLoading) {
+        return <div className={classes.center}>
+            <section className={classes.MealsLoading}>
+                <BarWave color="#8a2b06" />
+            </section>
+        </div>
+    }
     const mealsList = meals.map((meal) => <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
     return (
         <section className={classes.meals}>
